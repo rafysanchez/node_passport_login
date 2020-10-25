a282a516b54042f9e7b9c6af3341a0c4c8f2a9a4
rafael sanchez port <rafysanchez@hotmail.com>
2020-10-25 20:47:53 -0300

    adjs

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb+srv://rafysanchez:'+ encodeURIComponent('113001') + '@clusterinitial.6xzal.mongodb.net/dbTrad?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
